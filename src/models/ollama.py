e03d1c8d6e38b748c3ab3a85a6a550c8ab0b7793
EmilioGuillem <gusiem07@gmail.com>
2025-02-05 17:10:24 +0100

    v.0.0.1_beta

--- src/models/ollama.py
+++ src/models/ollama.py
@@ -9,6 +9,10 @@ from langchain.prompts import ChatPromptTemplate
 #get memory
 from langchain.chains.conversation.memory import ConversationBufferMemory
 from langchain.chains import conversation
+import os
+import datetime
+
+from pathlib import Path
     
 
 class OllamaChat:
@@ -27,6 +31,7 @@ class OllamaChat:
             self.content = [{'role':'user', 'content':qst}]
             self.memory = ConversationBufferMemory()
             self.messages=""
+            self.chat_history = []
     
     #Suprimir        
     # def getReponse(self, data:json):
@@ -54,18 +59,41 @@ class OllamaChat:
         
         self.resultQuery= reponse.message.content
         print(self.resultQuery)
-        
-         
+    
+    def append_context(self, new_file_path:Path):   
+         if new_file_path.is_file():
+            newfile = open(new_file_path)
+            content = newfile.read()
+            newfile.close
+            self.chat_history.append(content)
+            
     def chat_with_ollama_history(self, user_input):
-        messages=""
-        chat_history= []
+        #create context IA"
+        folder_path = Path("C:\\Users\\Emilio Guillem\\Documents\\GIT\\IA\\src\\context_db")
+        for file_path in folder_path.iterdir():
+            self.append_context(file_path)
+                
+                
         while True:
             user_input = input('Emilio: ')
-            # new_message = [{'role':'user', 'content':user_input}]
-            chat_history.append({'role':'user', 'content':user_input})
+
+            if user_input.lower().__contains__("Orbital, apaga") or user_input.lower().__contains__("stop conversation") or user_input.lower().__contains__("finaliza conversacion")or user_input.lower().__contains__("finaliza conversación")or user_input.lower().__contains__("cerrar"):
+                break
+            if user_input.lower().__contains__("base de datos") or user_input.lower().__contains__("database"):
+                if user_input.lower().__contains__("almacena") or user_input.lower().__contains__("insert") or user_input.lower().__contains__("guarda"):
+                    now = datetime.datetime.now()
+                    newFilePath = "C:\\Users\\Emilio Guillem\\Documents\\GIT\\IA\\src\\context_db\\context_"+str(now.strftime("%d%m%Y"))
+                    if os.path.exists(newFilePath):
+                        self.append_context(newFilePath)
+                        
+                    newfile  = open(newFilePath, "w+")
+                    newfile.write(''.join(str(x) for x in self.chat_history))
+                    newfile.close()
+                       
+            self.chat_history.append({'role':'user', 'content':user_input})
             response = ollama.chat(
                 model = self.model,
-                messages=chat_history,
+                messages=self.chat_history,
                 options={
                         'num_ctx': 4096,
                         'temperature': 0.7,
@@ -74,5 +102,6 @@ class OllamaChat:
             )
 
             # Add the response to the messages to maintain the history
-            chat_history.append(response['message'])
-            print(response.message.content + '\n')
+            self.chat_history.append(response['message'])
+            # self.chat_history.append({'role':'assistant', 'content':response['message']})
+            print("Orbital: " + response.message.content + '\n')
