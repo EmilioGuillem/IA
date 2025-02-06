0416b79b5f75813f7c0701a799ec092d82fe72c2
EmilioGuillem <gusiem07@gmail.com>
2025-02-06 22:32:20 +0100

    Update Update_model_ollama.py

--- src/models/Update_model_ollama.py
+++ src/models/Update_model_ollama.py
@@ -1,164 +1,56 @@
-from unsloth import FastLanguageModel
-import torch
-
-from models import Ollama
-max_seq_length = 2048 
-dtype = None 
-load_in_4bit = True 
-
-
-fourbit_models = [
-    "unsloth/Meta-Llama-3.1-8B-bnb-4bit",     
-    "unsloth/Meta-Llama-3.1-8B-Instruct-bnb-4bit",
-    "unsloth/Meta-Llama-3.1-70B-bnb-4bit",
-    "unsloth/Meta-Llama-3.1-405B-bnb-4bit",    
-    "unsloth/Mistral-Small-Instruct-2409",     
-    "unsloth/mistral-7b-instruct-v0.3-bnb-4bit",
-    "unsloth/Phi-3.5-mini-instruct",           
-    "unsloth/Phi-3-medium-4k-instruct",
-    "unsloth/gemma-2-9b-bnb-4bit",
-    "unsloth/gemma-2-27b-bnb-4bit",            
-
-    "unsloth/Llama-3.2-1B-bnb-4bit",           
-    "unsloth/Llama-3.2-1B-Instruct-bnb-4bit",
-    "unsloth/Llama-3.2-3B-bnb-4bit",
-    "unsloth/Llama-3.2-3B-Instruct-bnb-4bit",
-] 
-
-model, tokenizer = FastLanguageModel.from_pretrained(
-    model_name = "unsloth/Llama-3.2-3B-Instruct", 
-    max_seq_length = max_seq_length,
-    dtype = dtype,
-    load_in_4bit = load_in_4bit,
-)
-
-model = FastLanguageModel.get_peft_model(
-    model,
-    r = 16, #Suggested values 8, 16, 32, 64, 128
-    target_modules = ["q_proj", "k_proj", "v_proj", "o_proj",
-                      "gate_proj", "up_proj", "down_proj",],
-    lora_alpha = 16,
-    lora_dropout = 0, #0 is optimized
-    bias = "none",    #"none" is optimized
-    
-    use_gradient_checkpointing = "unsloth", 
-    random_state = 3407,
-    use_rslora = False,  #rank stabilized LoRA
-    loftq_config = None, 
-)
-
-from unsloth.chat_templates import get_chat_template
-
-tokenizer = get_chat_template(
-    tokenizer,
-    chat_template = "llama-3.2",
-)
-
-def formatting_prompts_func(examples):
-    convos = examples["conversations"]
-    texts = [tokenizer.apply_chat_template(convo, tokenize = False, add_generation_prompt = False) for convo in convos]
-    return { "text" : texts, }
-pass
-
 from datasets import load_dataset
-# dataset = load_dataset("mlabonne/FineTome-100k", split = "train")
-dataset = load_dataset("C:\\Users\\Emilio Guillem\\Documents\\GIT\\IA\\src\\context_db\\context", split = "train")
 
-from unsloth.chat_templates import standardize_sharegpt
-dataset = standardize_sharegpt(dataset)
-dataset = dataset.map(formatting_prompts_func, batched = True,)
+dataset = load_dataset("text", data_files="C:\\Users\\Emilio Guillem\\Documents\\GIT\\IA\\src\\context_db\\context.txt", encoding='latin-1', split="train")
+
+from transformers import AutoTokenizer
+
+tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-3.2-3B")
 
 
-from trl import SFTTrainer
-from transformers import TrainingArguments, DataCollatorForSeq2Seq
-from unsloth import is_bfloat16_supported
+def tokenize_function(examples):
+    tokenizer.pad_token = tokenizer.eos_token
+    return tokenizer(examples['text'], padding="max_length", truncation=True)
 
-trainer = SFTTrainer(
-    model = model,
-    tokenizer = tokenizer,
-    train_dataset = dataset,
-    dataset_text_field = "text",
-    max_seq_length = max_seq_length,
-    data_collator = DataCollatorForSeq2Seq(tokenizer = tokenizer),
-    dataset_num_proc = 2,
-    packing = False, 
-    args = TrainingArguments(
-        per_device_train_batch_size = 2,
-        gradient_accumulation_steps = 4,
-        warmup_steps = 5,
-        max_steps = 60,
-        learning_rate = 2e-4,
-        fp16 = not is_bfloat16_supported(),
-        bf16 = is_bfloat16_supported(),
-        logging_steps = 1,
-        optim = "adamw_8bit",
-        weight_decay = 0.01,
-        lr_scheduler_type = "linear",
-        seed = 3407,
-        output_dir = "outputs",
-    ),
+
+tokenized_datasets = dataset.map(tokenize_function, batched=True)
+
+# small_train_dataset = tokenized_datasets["train"].shuffle(seed=42).select(range(1000))
+# small_eval_dataset = tokenized_datasets["test"].shuffle(seed=42).select(range(1000))
+
+small_train_dataset = tokenized_datasets.shuffle(seed=42).select(range(1000))
+small_eval_dataset = tokenized_datasets.shuffle(seed=42).select(range(1000))
+
+#trainin pytorch model
+from transformers import AutoModelForSequenceClassification
+
+model = AutoModelForSequenceClassification.from_pretrained("google-bert/bert-base-cased", num_labels=5, torch_dtype="auto")
+from transformers import TrainingArguments
+
+training_args = TrainingArguments(output_dir="test_trainer")
+
+import numpy as np
+import evaluate
+
+metric = evaluate.load("accuracy")
+
+def compute_metrics(eval_pred):
+    logits, labels = eval_pred
+    predictions = np.argmax(logits, axis=-1)
+    return metric.compute(predictions=predictions, references=labels)
+
+from transformers import TrainingArguments, Trainer
+
+training_args = TrainingArguments(output_dir="test_trainer", eval_strategy="epoch")
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=small_train_dataset,
+    eval_dataset=small_eval_dataset,
+    compute_metrics=compute_metrics,
 )
 
-from unsloth.chat_templates import train_on_responses_only
-trainer = train_on_responses_only(
-    trainer,
-    instruction_part = "<|start_header_id|>user<|end_header_id|>\n\n",
-    response_part = "<|start_header_id|>assistant<|end_header_id|>\n\n",
-)
-
-trainer_stats = trainer.train()
-
-from unsloth.chat_templates import get_chat_template
-
-tokenizer = get_chat_template(
-    tokenizer,
-    chat_template = "llama-3.1",
-)
-FastLanguageModel.for_inference(model) # Enable native 2x faster inference
-
-messages = [
-    {"role": "user", "content": "Tu nombre es Orbital."},
-]
-inputs = tokenizer.apply_chat_template(
-    messages,
-    tokenize = True,
-    add_generation_prompt = True, # Must add for generation
-    return_tensors = "pt",
-).to("cuda")
-
-outputs = model.generate(input_ids = inputs, max_new_tokens = 64, use_cache = True,
-                         temperature = 1.5, min_p = 0.1)
-tokenizer.batch_decode(outputs)
-
-FastLanguageModel.for_inference(model) # Enable native 2x faster inference
-
-messages = [
-    {"role": "user", "content": "Buenos días, Orbital."},
-]
-inputs = tokenizer.apply_chat_template(
-    messages,
-    tokenize = True,
-    add_generation_prompt = True, # Must add for generation
-    return_tensors = "pt",
-).to("cuda")
-
-from transformers import TextStreamer
-text_streamer = TextStreamer(tokenizer, skip_prompt = True)
-_ = model.generate(input_ids = inputs, streamer = text_streamer, max_new_tokens = 128,
-                   use_cache = True, temperature = 1.5, min_p = 0.1)
-
-model.save_pretrained("lora_model") 
-tokenizer.save_pretrained("lora_model")
-model.save_pretrained_gguf("model", tokenizer, quantization_method = "f16")
-
-# Push to OLLAMA
-# ollama = Ollama()
-# ollama.push_to_hub(model_path="C:\\Users\\Emilio Guillem\\.ollama\\models\\manifests\\registry.ollama.ai\\library\\orbital:latest")
-ollama = Ollama()
-ollama.push_to_hub(
-    model_path="./your-model",
-    repo_name="your-username/your-model-repo",
-    token="your-huggingface-token"
-)
+trainer.train()
 
 
+#keras(https://huggingface.co/docs/transformers/training)
