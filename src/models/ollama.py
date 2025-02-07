f3d85fa4a2cd15f052c1fbeeea5d90fb5357731a
EmilioGuillem <129878319+EmilioGuillem@users.noreply.github.com>
2025-02-07 11:41:36 +0100

    v0.0.1_gamma2

--- src/models/ollama.py
+++ src/models/ollama.py
@@ -66,64 +66,75 @@ class OllamaChat:
          if new_file_path.is_file():
             newfile = open(new_file_path)
             content = newfile.read()
-            content = "Histórico de conversaciones anteriores : " + content
             newfile.close
             # self.chat_history.append(content)
-            self.chat_history_txt = ''.join(str(x) for x in self.chat_history)+"\n"+content
-    
+            self.chat_history_txt = content+"\n"+self.chat_history_txt
+
     def append_context_json(self, new_file_path:Path):
         if new_file_path.is_file():
             with open(new_file_path, 'r') as json_file:
                 data = json.load(json_file)
-            
+
             # Append new data
-            data.append(self.chat_history)
+            self.chat_history.append(data['content'])
 
             # Write updated data back to the file
-            with open(new_file_path, 'w') as file:
+            with open(new_file_path, 'w+') as file:
                 json.dump(data, file, indent=4)
 
             print("Data appended successfully!")
-            
+
+
     def context_db(self):
         #create context IA"
-        folder_path = Path("C:\\Users\\Emilio Guillem\\Documents\\GIT\\IA\\src\\context_db")
         file_path = Path("C:\\Users\\Emilio Guillem\\Documents\\GIT\\IA\\src\\context_db\\context.txt")
+        file_path_json = Path("C:\\Users\\Emilio Guillem\\Documents\\GIT\\IA\\src\\context_db\\context.json")
         # for file_path in folder_path.iterdir():
         self.append_context(file_path)
-            
-        self.chat_history.append({'role':'user', 'content':self.chat_history_txt})
+        self.append_context_json(file_path_json)
+
+        message_history = ({'role':'user', 'content':"Histórico de conversaciones anteriores: \n"+self.chat_history_txt})
+        message_history_json = ({'role':'user', 'content':self.chat_history_txt})
         response = ollama.chat(
             model = self.model,
-            messages=self.chat_history,
+            messages=message_history,
             options={
                     'num_ctx': 4096,
                     'temperature': 0.7,
                     'repeat_penalty':1.2
                 },
-        ) 
-    def chat_with_ollama_history(self, user_input):       
-        # self.context_db()       
-                
-        while True:
+        )
+        # response_json = ollama.chat(
+        #     model = self.model,
+        #     messages=message_history_json,
+        #     options={
+        #             'num_ctx': 4096,
+        #             'temperature': 0.7,
+        #             'repeat_penalty':1.2
+        #         },
+        # )
+    def chat_with_ollama_history(self, user_input):
+        # self.context_db()
+        now = datetime.datetime.now()
+        var_continue = True
+        while var_continue:
             user_input = input('User: ')
-            user_input = str(datetime.datetime.now().today().strftime("%d-%m-%Y %H:%M:%S")) + ": " + user_input
+            user_input = str(datetime.datetime.now().today().strftime("%d-%m-%Y %H:%M:%S")) + " - " + user_input
             if user_input.lower().__contains__("Orbital, apaga") or user_input.lower().__contains__("stop conversation") or user_input.lower().__contains__("finaliza conversacion")or user_input.lower().__contains__("finaliza conversación")or user_input.lower().__contains__("cerrar"):
-                break
+                var_continue= False;
             if user_input.lower().__contains__("base de datos") or user_input.lower().__contains__("database"):
                 if user_input.lower().__contains__("almacena") or user_input.lower().__contains__("insert") or user_input.lower().__contains__("guarda"):
-                    now = datetime.datetime.now()
-                    # newFilePath = Path("C:\\Users\\Emilio Guillem\\Documents\\GIT\\IA\\src\\context_db\\context_"+str(now.strftime("%d%m%Y")+".txt"))
-                    newFilePath = Path("C:\\Users\\Emilio Guillem\\Documents\\GIT\\IA\\src\\context_db\\context")
-                    # newFilePath = Path("C:\\Users\\Emilio Guillem\\Documents\\GIT\\IA\\src\\context_db\\context.json")
+                    newFilePath = Path("C:\\Users\\Emilio Guillem\\Documents\\GIT\\IA\\src\\context_db\\context_txt")
+                    newFilePath_json = Path("C:\\Users\\Emilio Guillem\\Documents\\GIT\\IA\\src\\context_db\\context.json")
                     if os.path.exists(newFilePath):
                         self.append_context(newFilePath)
-                        
+                        self.append_context_json(newFilePath_json)
+
                     newfile  = open(newFilePath, "w+")
-                    newfile.write(self.chat_history_txt+''.join(str(x) for x in self.chat_history))
+                    newfile.write(self.chat_history_txt)
                     newfile.close()
-                       
-            self.chat_history.append({'role':'user', 'content':user_input})
+
+            self.chat_history.append({'role':'user', 'content':str(datetime.datetime.now().today().strftime("%d-%m-%Y %H:%M:%S")) + " - " + user_input})
             response = ollama.chat(
                 model = self.model,
                 messages=self.chat_history,
@@ -135,6 +146,7 @@ class OllamaChat:
             )
 
             # Add the response to the messages to maintain the history
-            self.chat_history.append(response['message'])
-            # self.chat_history.append({'role':'assistant', 'content':response['message']})
+            # self.chat_history.append(response['message'])
+            self.chat_history.append({'role':'assistant', 'content': str(datetime.datetime.now().today().strftime("%d-%m-%Y %H:%M:%S")) + " - " + response.message.content})
+            self.chat_history_txt += "User: "+user_input+"\n"+"Orbital: "+ str(datetime.datetime.now().today().strftime("%d-%m-%Y %H:%M:%S")) + " - " + response.message.content
             print("Orbital: " + response.message.content + '\n')
