0452ef4da9f5fd3a27293cb67ca4bed3583893ed
EmilioGuillem <129878319+EmilioGuillem@users.noreply.github.com>
2025-03-13 11:22:00 +0100

    Update test_training.py

--- src/models/test_training.py
+++ src/models/test_training.py
@@ -1,117 +1,104 @@
-# Absolutely! Fine-tuning a language model like LLaMA 3.2B involves several steps. Below is a concise example of how you might fine-tune the model using Python and the Hugging Face Transformers library. This example assumes you have a dataset ready for fine-tuning.
-
-# Step 1: Install Required Libraries
-
-# First, ensure you have the necessary libraries installed:
-
-# pip install transformers datasets torch
-
-# Step 2: Load the Model and Dataset
-
-# Here's a basic script to load the model and dataset, and then fine-tune it:
-
-from pathlib import Path
+# from numba import jit, cuda
+import numpy as np
+from datasets import load_dataset
+import os
 import torch
 from transformers import Trainer, TrainingArguments
-from datasets import load_dataset
+from sklearn.model_selection import train_test_split
 
-# Load model directly
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from trl import SFTTrainer
 
-tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-3.2-3B", token="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")
-tokenizer.add_special_tokens
-tokenizer.pad_token= "<|end_of_text|>"
-tokenizer.padding_side="right"
-model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-3.2-3B", torch_dtype=torch.float32, device_map="auto")
-tokenizer.pad_token= tokenizer.eos_token
-# Load your dataset
-dataset = load_dataset("text", data_files="C:\\Users\\Emilio Guillem\\Documents\\GIT\\IA\\src\\context_db\\context.txt", encoding='latin-1', split="train")
-dataset.to_dict()
-# dataset_train, dataset_eval =dataset.train_test_split(test_size=0.2, seed=42)
-dataset = dataset.shuffle(seed=80)
-# Tokenize the dataset
-import nltk
-nltk.download('punkt')
-file_content = open("C:\\Users\\Emilio Guillem\\Documents\\GIT\\IA\\src\\context_db\\context.txt").read()
-tokens_dt = nltk.word_tokenize(file_content)
-def tokenize_function(examples):
-    tokens_dt = tokenizer(examples["text"], padding="max_length", truncation=True, max_length=128)
-    tokens=[
-        -100 if token == tokenizer.pad_token_id else token for token in tokens_dt
-    ]
-    return tokens
+from transformers import Wav2Vec2Processor
+from transformers import Wav2Vec2ForCTC
 
-tokenized_datasets = dataset.map(tokenize_function)
-tokenized_datasets = tokenized_datasets.remove_columns(['text'])
-model.train()
-# Set training arguments
-training_args = TrainingArguments(
-    output_dir="./results",
-    eval_strategy="steps",
-    eval_steps=40,
-    logging_steps=40,
-    save_steps=150,
-    learning_rate=2e-5,
-    per_device_train_batch_size=2,
-    per_device_eval_batch_size=2,
-    num_train_epochs=2,
-    fp16=False,
-    log_level="info",
-    weight_decay=0.01,
-    max_grad_norm=2
-)
-# from trl import SFTConfig, SFTTrainer
-# training_args = SFTConfig(
-#     output_dir="./results",
-#     optim="adamw_torch_fused",
-#     evaluation_strategy="steps",
-#     eval_steps=40,
-#     logging_steps=10,
-#     save_strategy="epoch",
-#     learning_rate=2e-4,
-#     per_device_train_batch_size=16,
-#     per_device_eval_batch_size=16,
-#     gradient_accumulation_steps=4,
-#     gradient_checkpointing = True,
-#     num_train_epochs=5,
-#     fp16=False,
-#     bf16=True,
-#     log_level="info",
-#     weight_decay=0.01,
-#     max_grad_norm=0.3,
-#     warmup_ratio=0.03,
-#     lr_scheduler_type="cosine",
-#     dataset_text_field="text",
-#     report_to="tensorboard",
-#     gradient_checkpointing_kwargs={"use_reentrant": False},
-# )
+def main():
+    path_to_save_model = 'C:\\Users\\Emilio\\Documents\\GitHub\\IA\\src\\llm\\audio_cassifier'
+    path_to_hggf_model = 'C:\\Users\\Emilio\\Documents\\GitHub\\IA\\src\\llm\\audio_classifier'
+    # Check if CUDA is available
+    print(torch.cuda.is_available()) # True if CUDA is available
 
-# Initialize the Trainer
-trainer = Trainer(
-    model=model,
-    args=training_args,
-    train_dataset=tokenized_datasets['train'],
-    eval_dataset=tokenized_datasets['test']
-)
+    # Get the number of GPUs available
+    print(torch.cuda.device_count()) # Number of GPUs
+
+    # Get the name of the current GPU
+    print(torch.cuda.get_device_name(0)) #
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    torch.cuda.empty_cache()
+
+    #Load the processor and model.
+    MODEL_NAME="carlosdanielhernandezmena/wav2vec2-large-xlsr-53-spanish-ep5-944h"
+    processor = Wav2Vec2Processor.from_pretrained(MODEL_NAME)
+    model = Wav2Vec2ForCTC.from_pretrained(MODEL_NAME)
+
+    #Load the dataset
+    from datasets import load_dataset, Audio
+    import evaluate
+    ds=load_dataset("ciempiess/ciempiess_test", split="test")
+    # dataset_train, dataset_eval = load_dataset("json", data_files="C:\\Users\\Emilio\\Documents\\GitHub\\IA\\src\\context_db\\context.json",encoding='latin1',  split=['train[:80%]', 'train[80%:]'])
+    #Downsample to 16kHz
+    ds = ds.cast_column("audio", Audio(sampling_rate=16_000))
+
+    # Process the dataset
+    def prepare_dataset(batch):
+        audio = batch["audio"]
+        #Batched output is "un-batched" to ensure mapping is correct
+        batch["input_values"] = processor(audio["array"], sampling_rate=audio["sampling_rate"]).input_values[0]
+        with processor.as_target_processor():
+            batch["labels"] = processor(batch["normalized_text"]).input_ids
+        return batch
+    ds = ds.map(prepare_dataset, remove_columns=ds.column_names,num_proc=1)
+
+    #Define the evaluation metric
+    import numpy as np
+    wer_metric = evaluate.load("wer")
+    def compute_metrics(pred):
+        pred_logits = pred.predictions
+        pred_ids = np.argmax(pred_logits, axis=-1)
+        pred.label_ids[pred.label_ids == -100] = processor.tokenizer.pad_token_id
+        pred_str = processor.batch_decode(pred_ids)
+        #We do not want to group tokens when computing the metrics
+        label_str = processor.batch_decode(pred.label_ids, group_tokens=False)
+        wer = wer_metric.compute(predictions=pred_str, references=label_str)
+        return {"wer": wer}
+
+    #Do the evaluation (with batch_size=1)
+    model = model.to(torch.device("cuda"))
+    def map_to_result(batch):
+        with torch.no_grad():
+            input_values = torch.tensor(batch["input_values"], device="cuda").unsqueeze(0)
+            logits = model(input_values).logits
+        pred_ids = torch.argmax(logits, dim=-1)
+        batch["pred_str"] = processor.batch_decode(pred_ids)[0]
+        batch["sentence"] = processor.decode(batch["labels"], group_tokens=False)
+        return batch
+    results = ds.map(map_to_result,remove_columns=ds.column_names)
+
+    #Compute the overall WER now.
+    print("Test WER: {:.3f}".format(wer_metric.compute(predictions=results["pred_str"], references=results["sentence"])))
 
 
-# Fine-tune the model
-trainer.train()
+    model.save_pretrained(path_to_save_model)
+    processor.save_pretrained(path_to_save_model)
 
-# Step 3: Save the Fine-Tuned Model
+    #save gguf for ollama serve
+    # Step 3: Save the Fine-Tuned Model
 
-# After training, you can save the fine-tuned model:
+    # After training, you can save the fine-tuned model:
 
-# model.save_pretrained("C:/Users/Emilio Guillem/Documents/GIT/IA/src/llm/fine_tuned_llama")
-# tokenizer.save_pretrained("C:/Users/Emilio Guillem/Documents/GIT/IA/src/llm/fine_tuned_llama")
+    # model.save_pretrained("C:/Users/Emilio Guillem/Documents/GIT/IA/src/llm/fine_tuned_llama")
+    # tokenizer.save_pretrained("C:/Users/Emilio Guillem/Documents/GIT/IA/src/llm/fine_tuned_llama")
 
-# Notes:
-# Dataset: Replace "path_to_your_dataset" with the actual path or name of your dataset.
-# Hyperparameters: Adjust the hyperparameters (e.g., learning rate, batch size, number of epochs) as needed for your specific use case.
-# Hardware: Ensure you have the necessary hardware (e.g., GPU) to handle the fine-tuning process, especially for large models like LLaMA 3.2B.
+    # Notes:
+    # Dataset: Replace "path_to_your_dataset" with the actual path or name of your dataset.
+    # Hyperparameters: Adjust the hyperparameters (e.g., learning rate, batch size, number of epochs) as needed for your specific use case.
+    # Hardware: Ensure you have the necessary hardware (e.g., GPU) to handle the fine-tuning process, especially for large models like LLaMA 3.2B.
 
-# Feel free to adapt this script to better fit your needs. Happy fine-tuning!
-import os
-os.environ['HF_TOKENIZER'] = "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"
-model.push_to_hub("Emiliogs/orbital", use_auth_token=os.getenv("HF_TOKEN"))
-tokenizer.push_to_hub("Emiliogs/orbital", use_auth_token=os.getenv("HF_TOKEN"))
+    # Feel free to adapt this script to better fit your needs. Happy fine-tuning!
+    # import os
+    # os.environ['HF_TOKENIZER'] = "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"
+    # model.push_to_hub("Emiliogs/orbital", use_auth_token=os.getenv("HF_TOKEN"))
+    # tokenizer.push_to_hub("Emiliogs/orbital", use_auth_token=os.getenv("HF_TOKEN"))
+
+
+if __name__ == "__main__":
+    main()
